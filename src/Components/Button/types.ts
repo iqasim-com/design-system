e30d5329c946cf640d47aa2566a3b185bf366ef4
Muhammad Qasim <qasim.tanxe@gmail.com>
2025-04-24 03:10:01 +0500

    Deleted build, updated css classes and variables, user is able to set different variants

--- src/Components/Button/types.ts
+++ src/Components/Button/types.ts
@@ -3,6 +3,6 @@ import React from 'react';
 export interface ButtonProps {
   children?: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'danger' | 'success' | 'warning' | 'info' | 'light' | 'dark' | 'link';
   click: (event: React.MouseEvent) => void;
 }
